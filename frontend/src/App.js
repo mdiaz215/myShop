325625a867d2512fceca70bc5a278e9bb0234c8b
marisadiaz <Risa.diaz@gmail.com>
2024-09-08 12:57:54 -0400

    React project setup

---
+++ frontend/src/App.js
@@ -0,0 +1,19 @@
+import { Container } from 'react';
+import Footer from './components/Footer';
+import Header from './components/Header';
+
+const App = () => {
+  return (
+    <>
+      <Header />
+      <main>
+        <Container>
+          <h1>Welcome to my shop!</h1>
+        </Container>
+      </main>
+      <Footer />
+    </>
+  );
+}
+
+export default App;
