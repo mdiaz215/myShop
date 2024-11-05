1d2dfb79377432f5ba2d6751e222f88bd85b6a27
marisadiaz <Risa.diaz@gmail.com>
2024-11-05 10:24:13 -0500

    fixed orderRoutes

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,11 @@
-import express from 'express';
-import dotenv from 'dotenv';
+import express from 'express'
+import dotenv from 'dotenv'
+import path from 'path'
 import connectDB from './config/db.js'
 import productRoutes from './routes/productRoutes.js'
 import userRoutes from './routes/userRoutes.js'
-import errorHandler from './middleware/errorMiddleware.js';
+import orderRoutes from './routes/orderRoutes.js'
+import errorHandler from './middleware/errorMiddleware.js'
 
 const app = express()
 dotenv.config()
@@ -12,7 +14,18 @@ connectDB()
 app.use(express.json())
 app.use('/api/products', productRoutes)
 app.use('/api/users', userRoutes)
-app.get('/api/config/paypal', (req, res) => res.send(process.env.PAYPAL_CLIENT_ID))
+app.use('/api/orders', orderRoutes)
+app.get('/api/config/paypal', (req, res) => 
+  res.send(process.env.PAYPAL_CLIENT_ID)
+)
+
+const __dirname = path.resolve()
+if (process.env.NODE_ENV === 'production'){
+  app.use(express.static(path.join(__dirname, '/frontend/build')))
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+  })
+}
 
 app.use(errorHandler)
 const port = process.env.PORT
