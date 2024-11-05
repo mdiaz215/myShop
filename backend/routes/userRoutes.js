6609283786907b48491c16963e9b5cf0c7b65418
marisadiaz <Risa.diaz@gmail.com>
2024-11-05 10:21:03 -0500

    fixed userRoutes

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import { authUser } from '../controllers/userController.js'
+import { authUser, registerUser, getUserProfile, updateUserProfile } from '../controllers/userController.js'
 import { protect } from '../middleware/authMiddleware.js'
 
 const router = express.Router()
